fde1a1ddaf190439bb198db5cb12ca83abbba896
Mohamed Mohamed Ahmed <66917039+WDpad753@users.noreply.github.com>
2022-09-15 20:02:47 +0100

    Add files via upload

---
+++ GPS_Tracker/sketch_nov12a/sketch_nov12a.ino
@@ -0,0 +1,87 @@
+#include <AltSoftSerial.h>
+
+//Make sure to install the adafruit GPS library from https://github.com/adafruit/Adafruit-GPS-Library
+
+#include <Adafruit_GPS.h> //Load the GPS Library. Make sure you have installed the library form the adafruit site above
+#include <SoftwareSerial.h> //Load the Software Serial Library. This library in effect gives the arduino additional serial ports
+#include <Time.h>
+#include <TimeLib.h>
+#include <CmdMessenger.h>
+#include <RTCTimer.h>
+#include <MicroNMEA.h>
+
+// If you're using a GPS module:
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// If using software serial (sketch example default):
+//   Connect the GPS TX (transmit) pin to Digital 3
+//   Connect the GPS RX (receive) pin to Digital 2
+// If using hardware serial (e.g. Arduino Mega):
+//   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
+//   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
+
+// If you're using the Adafruit GPS shield, change 
+// SoftwareSerial mySerial(3, 2); -> SoftwareSerial mySerial(8, 7);
+// and make sure the switch is set to SoftSerial
+
+// If using software serial, keep this line enabled
+// (you can change the pin numbers to match your wiring):
+SoftwareSerial mySerial(3, 2); //Initialize SoftwareSerial, and tell it you will be connecting through pins 2 and 3
+
+// If using hardware serial (e.g. Arduino Mega), comment out the
+// above SoftwareSerial line, and enable this line instead
+// (you can change the Serial number to match your wiring):
+
+//HardwareSerial mySerial = Serial1;
+
+Adafruit_GPS GPS(&mySerial); //Create GPS object
+
+String NMEA1;  //We will use this variable to hold our first NMEA sentence
+String NMEA2;  //We will use this variable to hold our second NMEA sentence
+char c;       //Used to read the characters spewing from the GPS module
+
+void setup()
+{
+  Serial.begin(115200);  //Turn on the Serial Monitor
+  GPS.begin(9600);       //Turn GPS on at baud rate of 9600
+  GPS.sendCommand("$PGCMD,33,0*6D"); // Turn Off GPS Antenna Update
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Tell GPS we want only $GPRMC and $GPGGA NMEA sentences
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);    // Request updates on antenna status, comment out to keep quiet
+  delay(1000);  //Pause
+}
+
+
+void loop()                     // run over and over again
+{
+  readGPS();  //This is a function we define below which reads two NMEA sentences from GPS
+
+}
+void readGPS() { //This function will read and remember two NMEA sentences from GPS
+  clearGPS();    //Serial port probably has old or corrupt data, so begin by clearing it all out
+  while (!GPS.newNMEAreceived()) { //Keep reading characters in this loop until a good NMEA sentence is received
+    c = GPS.read(); //read a character from the GPS
+  }
+  GPS.parse(GPS.lastNMEA());  //Once you get a good NMEA, parse it
+  NMEA1 = GPS.lastNMEA();    //Once parsed, save NMEA sentence into NMEA1
+  while (!GPS.newNMEAreceived()) { //Go out and get the second NMEA sentence, should be different type than the first one read above.
+    c = GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+  NMEA2 = GPS.lastNMEA();
+  Serial.println(NMEA1);
+  Serial.println(NMEA2);
+  Serial.println("");
+}
+void clearGPS() {  //Since between GPS reads, we still have data streaming in, we need to clear the old data by reading a few sentences, and discarding these
+  while (!GPS.newNMEAreceived()) {
+    c = GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+  while (!GPS.newNMEAreceived()) {
+    c = GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+
+}
+
